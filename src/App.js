fc18db7d9df2b22b033d69411c2d1354f23c1d93
hyennii <alqkf@icloud.com>
2025-06-26 23:11:17 +0900

    컴포넌트, Props 이해

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
+import WelcomeProps from './WelcomeProps';
 
 function App() {
-  const name = "리액트"
-  return (
+  return(
     <>
-      {/* 삼항 연산자 조건식 */}
-      {name === '리액트' ? <h1>리액트입니다.</h1> : <h2>리액트가 아닙니다.</h2>}
-
-      {/* AND 연산자 조건식 */}
-      {name === '리액트' && <h1>리액트입니다.</h1>}
+      <WelcomeProps name="abc"/>
     </>
-  );
+  )
 }
 
 export default App;
---
+++ src/WelcomeProps.js
@@ -0,0 +1,5 @@
+function WelcomeProps(props) {
+  return <h1>Hello, {props.name}</h1>
+}
+
+export default WelcomeProps
